3fe130b7f46e05dd00500b88e27e90cbef92722b
Raymond Zeng <raymondzeng@live.com>
2014-01-20 16:13:11 -0500

    fixed pagination, included smallcss

--- static/scripts.js
+++ static/scripts.js
@@ -82,8 +82,31 @@ $(document).ready(function(){
 	    currentPage++;
 	    $('#resa'+(currentPage)+' a').click();
 	}
-    });
+    });    
 
+
+
+    var move = function() {
+        var st = $(window).scrollTop() + 50;
+        var ot = $("#scroll-anchor").offset().top;
+        var s = $(".pagination");
+        if(st > ot) {
+            s.css({
+                position: "fixed",
+                top: "50px"
+            });
+	    $("#scroll-anchor").css('height', '51px');
+        } else {
+            if(st <= ot) {
+                s.css({
+                    position: "relative",
+                    top: "auto"
+                });
+            }
+        }
+    };
+    $(window).scroll(move);
+    move();
 });
 
 function getPreview(){
@@ -117,3 +140,5 @@ function getPreview(){
 	}
     });
 }
+
+
