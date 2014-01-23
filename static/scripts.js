4444c917ca222350625cb1e36ef40d1a5de96ebe
Raymond Zeng <raymondzeng@live.com>
2014-01-22 21:56:12 -0500

    m

--- static/scripts.js
+++ static/scripts.js
@@ -46,16 +46,6 @@ $(document).ready(function(){
 	$('#settings').submit();
     });
     
-    var _gaq = _gaq || [];
-    _gaq.push(['_setAccount', 'UA-47415608-1']);
-    _gaq.push(['_trackPageview']);
-    
-    (function() {
-	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-    })();
-    
     var num = 1;
     $('.restable').each(function(){
 	$(this).attr('id',"res"+num);
